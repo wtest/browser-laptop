8be2858d9c284e1e9e01c8c18722c55b2ad2d5ec
yan <yan@mit.edu>
2016-06-20 19:13:32 -0700

    Add css and text for flash placeholder

--- js/about/flashPlaceholder.js
+++ js/about/flashPlaceholder.js
@@ -5,10 +5,34 @@
 const React = require('react')
 const ImmutableComponent = require('../components/immutableComponent')
 
+require('../../less/about/flash.less')
+
 class FlashPlaceholder extends ImmutableComponent {
+  constructor () {
+    super()
+    this.state = {
+      siteOrigin: 'foo',
+      sourceOrigin: 'bar'
+    }
+  }
+
   render () {
+    // TODO: Localization doesn't work due to CORS error from inside iframe
+    const flashRightClick = 'Right-click to run Adobe Flash'
+    const flashSubtext = `from ${this.state.sourceOrigin} on ${this.state.siteOrigin}.`
+    const flashExpirationText = 'Approvals expire 7 days after last site visit.'
     return <div>
-    {'hello world'}
+      <div className='flashMainContent'>
+        <img src='img/bravePluginAlert.png' />
+        <div id='flashRightClick'>{flashRightClick}</div>
+        <div className='flashSubtext' data-l10n-args={JSON.stringify({
+          source: this.state.sourceOrigin,
+          site: this.state.siteOrigin
+        })}>{flashSubtext}</div>
+      </div>
+      <div className='flashFooter'>
+        {flashExpirationText}
+      </div>
     </div>
   }
 }
